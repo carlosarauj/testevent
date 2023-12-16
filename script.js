e8cf33c105550651d8b9f3634055ddbeeb4ec6eb
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2023-12-16 09:48:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let test = document.querySelector('#test')
+
+test.addEventListener('click', clique)
+
+function clique() {
+    alert('oi')
+}
+
+let test2 = document.querySelector('#test2')
+
+test2.addEventListener('click', clique2)
+
+function clique2() {
+    alert('ola')
+}
